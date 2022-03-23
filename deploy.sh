70f6a431474b39a5ae8d099eb90b5b963738b932
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-03-23 20:20:19 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add .
 git commit -m 'Deploy'
 git remote add origin https://github.com/IvanValovyi/portfolio-site.git
-# git push origin --delete gh-pages
-# git branch gh-pages
+git push origin --delete gh-pages
+git branch gh-pages
 git push origin gh-pages
