5183f61a08e2bca570b0ba20a1315d54bf46766e
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-03-23 17:43:30 +0200

    Init

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer>
+        <p>c 2022. All right reserved (actualy not).</p>
+    </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <div class="profile">
+            <img src="~/assets/images/my_photo.jpg" alt="Face">
+            <div class="info">
+                <h1 class="name">Ivan Valovyi</h1>
+                <p class="position">Frontend developer</p>
+            </div>
+        </div>
+
+        <nav>
+            <ul class="main_links">
+                <li>
+                    <nuxt-link to="/about">About</nuxt-link>
+                </li><li>
+                    <nuxt-link to="/about">Contacts</nuxt-link>
+                </li><li>
+                    <nuxt-link to="/about">Skills</nuxt-link>
+                </li><li>
+                    <nuxt-link to="/about">Projects</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+
+        <ul class="other_links">
+            <li>
+                <a href="#">Living examples of works</a>
+            </li><li>
+                <a href="#">Order a website</a>
+            </li><li>
+                <a href="#">Write me</a>
+            </li>
+        </ul>
+    </header>
+</template>
