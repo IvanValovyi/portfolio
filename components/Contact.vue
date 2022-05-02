aa6dd5bbd81fd88589fb7661971a5c7d4a4e2f16
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-05-02 16:37:54 +0300

    Deploy

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,14 +2,14 @@
     <div class="contact" id="contact">
         <h2 class="title">Contact</h2>
         <div class="contact_list">
-            <a href="https://t.me/ivan_valovyi_37" class="one_contact">
+            <a href="https://t.me/ivan_valovyi_37" class="one_contact" target="_blank">
                 <svg>
                     <use xlink:href='~/assets/images/sprite.svg#telegram'></use>
                 </svg>
                 <p class="subtitle">Telegram</p>
             </a>
 
-            <a href="mailto:ivan.valovyi.ffs@gmail.com" class="one_contact">
+            <a href="mailto:ivan.valovyi.ffs@gmail.com" class="one_contact" target="_blank">
                 <svg>
                     <use xlink:href='~/assets/images/sprite.svg#gmail'></use>
                 </svg>
