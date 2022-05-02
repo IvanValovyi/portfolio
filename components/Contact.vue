b1e5b59240edfe7a861b5e40223fd9ad59140208
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-05-02 16:28:49 +0300

    Deploy

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,26 +2,26 @@
     <div class="contact" id="contact">
         <h2 class="title">Contact</h2>
         <div class="contact_list">
-            <a href="#" class="one_contact">
+            <a href="t.me/@ivan_valovyi_37" class="one_contact">
                 <svg>
                     <use xlink:href='~/assets/images/sprite.svg#telegram'></use>
                 </svg>
                 <p class="subtitle">Telegram</p>
             </a>
 
-            <a href="#" class="one_contact">
+            <a href="mailto:ivan.valovyi.ffs@gmail.com" class="one_contact">
                 <svg>
                     <use xlink:href='~/assets/images/sprite.svg#gmail'></use>
                 </svg>
                 <p class="subtitle">Gmail</p>
             </a>
 
-            <a href="#" class="one_contact">
+            <nuxt-link to="/chat" class="one_contact">
                 <svg>
                     <use xlink:href='~/assets/images/sprite.svg#chat'></use>
                 </svg>
                 <p class="subtitle">Write me</p>
-            </a>
+            </nuxt-link>
         </div>
     </div>
 </template>
