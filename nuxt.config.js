16cdf657b52727fadd4268f004d358bc92a3d4bc
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-07-04 15:30:32 +0300

    Deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Ivan Valovyi Portfolio website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: './my_photo.png' }
